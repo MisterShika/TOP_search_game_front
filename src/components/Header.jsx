b1f9ee5b7bb3f7517a52fb79d0c83179fc5911e2
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-04-21 17:08:02 +0900

    Image X and Y data can now be acquired on click.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header () {
+
+    return(
+        <header>
+            Header is here.
+        </header>
+    );
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import Header from '../components/Header';
+import waldo from '../assets/waldo.jpg'
+
 function Home () {
 
+    const imageClick = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const xCoordinate = (x / rect.width) * 100;
+        const yCoordinate = (y / rect.height) * 100;
+
+        const percentX = parseFloat(xCoordinate.toFixed(4));
+        const percentY = parseFloat(yCoordinate.toFixed(4));
+
+        console.log(percentX);
+        console.log(percentY);
+    };
+
     return (
-        <>
+        <main>
+            <Header />
             Hello World
-        </>
+            <img 
+            src={waldo}
+            onClick = {imageClick}
+            alt="Waldo" />
+        </main>
     )
 }
 
