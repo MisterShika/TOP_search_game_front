1776e82e55ce01bffdde76c1f8afd31398dd81ec
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-04-21 17:34:46 +0900

    Can now detect if point is within designated rectangle.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import Header from '../components/Header';
 import waldo from '../assets/waldo.jpg'
+import {useState, useEffect} from 'react';
 
 function Home () {
+    const [locationCoord, setLocationCoord] = useState([
+        {x: 64.0137, y: 89.7059},
+        {x: 68.2256, y: 90.0735},
+        {x: 68.7691, y: 97.2426},
+        {x: 63.8778, y: 97.0588}
+    ]);
+
+    function isPointInQuad(px, py) {
+        const cross = (ax, ay, bx, by) => ax * by - ay * bx;
+
+        for (let i = 0; i < 4; i++) {
+            const a = locationCoord[i];
+            const b = locationCoord[(i + 1) % 4];
+            const edgeX = b.x - a.x;
+            const edgeY = b.y - a.y;
+            const pointX = px - a.x;
+            const pointY = py - a.y;
+
+            if (cross(edgeX, edgeY, pointX, pointY) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     const imageClick = (e) => {
         const rect = e.target.getBoundingClientRect();
@@ -14,8 +39,9 @@ function Home () {
         const percentX = parseFloat(xCoordinate.toFixed(4));
         const percentY = parseFloat(yCoordinate.toFixed(4));
 
-        console.log(percentX);
-        console.log(percentY);
+        let blah = {x: percentX, y: percentY};
+
+        console.log(isPointInQuad(blah.x, blah.y));
     };
 
     return (
@@ -23,9 +49,9 @@ function Home () {
             <Header />
             Hello World
             <img 
-            src={waldo}
-            onClick = {imageClick}
-            alt="Waldo" />
+                src={waldo}
+                onClick = {imageClick}
+                alt="Waldo" />
         </main>
     )
 }
